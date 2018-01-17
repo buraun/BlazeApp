f179ef73c4d08ea540c4baecd65ed4c4fd50b5bf
barraud-Samuel <samuel.barraud@hetic.net>
2018-01-17 11:18:47 +0100

    architecture of the project

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,46 +1,37 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+// Swiping
+    document.addEventListener('swipeleft', function(event) {
+        if (event.target.matches('#detect-area')) {
+            console.log('Swipe left is detected.');
+        }
+    });
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+    document.addEventListener('swiperight', function(event) {
+        if (event.target.matches('#detect-area')) {
+            console.log('Swipe right is detected.');
+        }
+    });
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    //choose random images for the home
+    var body = document.querySelector('body');
+    var path = '/';
+    var nameimg =['test1.jpg','test2.png','test3.jpg','test4.jpg','test5.jpg'];
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    function displayImage() {
+        var num = Math.floor(Math.random() * (nameimg.length));
+        console.log(nameimg[num]);
+        body.style.background = "url(" + nameimg[num] + ")";
     }
-};
+    displayImage();
 
-app.initialize();
+//change pages home to swap
+document.addEventListener('init', function(event) {
+    var page = event.target;
+
+    if (page.id === 'home') {
+        page.querySelector('#push-button').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('container.html', {data: {title: 'container'}});
+        };
+    } else if (page.id === 'container') {
+        //page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+    }
+});
